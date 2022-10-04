347a9bcac70572d8875c71d7723edafdc3ad06a6
L-Bailey42 <114741918+L-Bailey42@users.noreply.github.com>
2022-10-04 15:03:13 +0100

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,79 @@
+#Task 1
+total = 100
+print("The total is", total)
+
+
+#Task 2
+total = total + 99
+print("\nThe total is now", total)
+
+
+#Task 3
+total = total - 1
+print("\nThe total is", total)
+
+total = total * 4
+print("\nThe total is", total)
+
+total = total / 2
+print("\nThe total is", total)
+
+
+#Task 4
+total = 98.2
+count = 5
+average = (total+count)/2
+print("\nThe average is", average)
+
+
+#Task 7
+print("\nLuke Bailey")
+print("Apparently I'm meant to upload my address to github...")
+print("...and my contact information")
+
+
+#Task 8
+age = input("\nEnter your age")
+age = int(age)
+print("in one year you will be", age + 1)
+
+
+#Task 9
+print("\nThis text includes characters such as \'\\\' \'\"\' and \"\'\",\n\tThis is a new line that starts with a tab\n\t\tThis new line starts with two tabs")
+
+
+#Task 10
+print("\n\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\nHello there!\n\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n")
+
+
+#Task 11
+print("""This text spans three lines,
+and includes both single ('),
+and double quotes (").""")
+
+#Task 12
+surname = "Palin"
+last = surname[-2]
+
+#Task 13
+surname = "Palin"
+end = surname[1:]
+print("\n", end)
+
+#Task 14
+surname = "Palin"
+start = surname[:-1]
+print("\n", start)
+
+#Task 15
+primes = [2, 3, 5, 7, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+print("\n", primes[4:])
+
+#Task 16
+names = ["Tim","Bill","Graeme","Terry, G."]
+names[-1:0] = ["Luke","Bailey"]
+print(names)
+
+#Task 17
+nums = [1,2,3]*5
+print(nums)
